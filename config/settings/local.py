54a5e1b62824df63aa6dd6d5b9d1bb3392429d23
Oscar L. aka moas <lasm.landry@gmail.com>
2016-05-28 00:33:30 +0200

    config of django debug toolbar

--- config/settings/local.py
+++ config/settings/local.py
@@ -13,5 +13,26 @@ DATABASES = {
 }
 
 THIRD_PARTY_APPS += (
-    'django-debug-toolbar',
+    'debug_toolbar',
 )
+
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+
+# debug toolbar config
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
